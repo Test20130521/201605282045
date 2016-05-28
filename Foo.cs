b2ad8f67873544b4528ed86254d7d4ec83e60a80
test201605281914 <test201605281914@gmail.com>
2016-05-28 23:40:50 +0300

    dev

---
+++ Foo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApplication44
+{
+    class Foo
+    {
+        public void M1(DateTime? date)
+        {
+            var builder = new StringBuilder(@"
+SELECT PostId, Text,  CreationDate
+FROM Post
+WHERE 1 = 1");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # 201605282045
 Description1
+
+[Description3](Foo.cs#L10) Description2  
+![Code сoverage](/r1.png?raw=true "Code сoverage")
+
+1
+2
